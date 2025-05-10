f1ab79a32efe854e1fbea85c4dc5b009f240aa38
kirkgriffin <krkgrffntn@gmail.com>
2025-05-10 15:27:35 +0800

    feat(landing page) added closing tags

--- index.php
+++ index.php
@@ -3,28 +3,31 @@
 <head>
     <title>Activity</title>
 </head>
-<body>
-    <?php
-    #Declarations
-    $name = "Kirk";
-    $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    <body>
+        <?php
+        #Declarations
+        $name = "Kirk";
+        $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-    echo "<h1>Welcome, $name!</h1>";
-    echo "<p>Here is your weekly routine: </p>";
+        echo "<h1>Welcome, $name!</h1>";
+        echo "<p>Here is your weekly routine: </p>";
 
-    //loops
-    for($i=0; $i<count($days); $i++){
-        $day = $days[$i];
-        echo "<div class = 'day-box>";
-        echo "<b>$day: </b>";
+        //loops
+        for($i=0; $i<count($days); $i++){
+            $day = $days[$i];
+            echo "<div class = 'day-box>";
+            echo "<b>$day: </b>";
 
-        //conditions
-        if($day == "Saturday" || $day == "Sunday"){
-            echo "Rest day!";
+            //conditions
+            if($day == "Saturday" || $day == "Sunday"){
+                echo "Rest day!";
+            }
+            else{
+                echo "Attend your class!";
+            }
+
+            echo "</div>";
         }
-        else{
-            echo "Attend your class!";
-        }
-
-        echo "</div>";
-    }
+        ?>
+    </body>
+</html>
