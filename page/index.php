620ca62370f9913318244569aaa19eeea6851988
kirkgriffin <krkgrffntn@gmail.com>
2025-05-10 15:55:56 +0800

    feat(tasks page) added variable declarations

--- page/index.php
+++ page/index.php
@@ -1,3 +1,9 @@
 <!DOCTYPE html>
 <html>
-    
+    <head>
+        <title>Tasks</title>
+    </head>
+    <body>
+        <?php
+        //declarations
+        $tasks = ["Wake up", "Eat breakfast", "Study", "Eat lunch", "Play games", "Sleep"];
