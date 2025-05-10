0f00f8e11048c2210125005014322701910c41e3
kirkgriffin <krkgrffntn@gmail.com>
2025-05-10 15:19:58 +0800

    feat(landing page) added variable declarations

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Activity</title>
+</head>
+<body>
+    <?php
+    #Declarations
+    $name = "Kirk";
+    $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    
