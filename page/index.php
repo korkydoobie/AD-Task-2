f81323bacefe09aeb03352c3921681852c5b57c2
kirkgriffin <krkgrffntn@gmail.com>
2025-05-10 16:00:36 +0800

    feat(tasks page) added closing tags

--- page/index.php
+++ page/index.php
@@ -5,14 +5,17 @@
     </head>
     <body>
         <?php
-        //declarations
-        $tasks = ["Wake up", "Eat breakfast", "Study", "Eat lunch", "Play games", "Sleep"];
+            //declarations
+            $tasks = ["Wake up", "Eat breakfast", "Study", "Eat lunch", "Play games", "Sleep"];
 
-        echo "<h1>Today's Tasks: </h1>";
-        echo "<ul>";
+            echo "<h1>Today's Tasks: </h1>";
+            echo "<ul>";
 
-        //loop
-        for($i=0; $i<count($tasks); $i++){
-            echo "<li>".$tasks[$i]."</li>";
-        }
-        echo "</ul>";
+            //loop
+            for($i=0; $i<count($tasks); $i++){
+                echo "<li>".$tasks[$i]."</li>";
+            }
+            echo "</ul>";
+        ?>
+    </body>
+</html>
