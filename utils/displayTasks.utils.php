cbcf754f9f7ff066c2b79417fc8d2ce64d953bd0
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 23:03:50 +0800

    refactor(display tasks util) changed code structure

--- utils/displayTasks.utils.php
+++ utils/displayTasks.utils.php
@@ -1,18 +1,17 @@
 <?php
-    function displayTasks(array $days, array $tasks){
-       echo '<div class="cards">';
-       foreach ($days as $day){
-        echo '<div class="card" onclick="toggleTasks(\'' . $day . '\')">';
-        echo '<h2>'.$day.'</h2>';
-
-        echo '<ul id="'.$day.'" class="task-list hidden">';
-        foreach ($tasks[$day] as $task){
+    function displayTasks(array $days, array $tasks) {
+    echo '<div class="cards">';
+    foreach ($days as $day) {
+        $safeDay = $day;
+        echo '<div class="card">';
+        echo '<h2>'.$safeDay.'</h2>';
+        echo '<ul style="display:none">';
+        foreach ($tasks[$day] as $task) {
             echo '<li>'.$task.'</li>';
         }
         echo '</ul>';
         echo '</div>';
     }
-
     echo '</div>';
 }
 ?>
