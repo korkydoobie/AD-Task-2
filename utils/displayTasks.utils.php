2e040e22b6eff3cda38a85db7d863767eea8e90f
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 19:51:14 +0800

    refactor(display task util) changed display style of tasks

--- utils/displayTasks.utils.php
+++ utils/displayTasks.utils.php
@@ -1,11 +1,17 @@
 <?php
-    function displayTasks($day, $taskPerDay){
-        echo "<h2>$day's Tasks: </h2>";
-        echo "<ul>";
-
-        foreach($taskPerDay[$day] as [$task]){
-            echo "<li> $task</li>";
+    function displayTasks(array $day, array $tasks){
+       echo '<div class="cards">';
+       foreach ($days as $day){
+        echo '<div class"card" onclick="toggleTasks(\'' . $day . '\')">';
+        echo "<h2 $day</h2>";
+        echo '<ul id="' . $day . '"class="task-list hidden">';
+        
+        foreach ($tasks[$day] as $task){
+            echo "<li>$task</li>";
         }
-        echo "</ul>";
+        echo '</ul>';
+        echo '</div>';
+       }
+       echo '</div>';
     }
 ?>
