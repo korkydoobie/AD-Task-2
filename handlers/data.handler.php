5cf64b5a1484180d3c83634140f1dfef9ea13551
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 18:16:28 +0800

    refactor(tasks page) transferred array declaration in tasks page to data handler and changed to dictionary

--- handlers/data.handler.php
+++ handlers/data.handler.php
@@ -1,2 +1,12 @@
 <?php
     $days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+    $tasks = [
+        "Monday" => ["Wake up", "Take a bath", "Go to school", "Eat lunch", "Go home", "Study", "Sleep"],
+        "Tuesday" => ["Wake up", "Take a bath", "Go to online class", "Eat lunch", "Study", "Sleep"],
+        "Wednesday" => ["Wake up", "Take a bath", "Go to school", "Eat lunch", "Go home", "Study", "Sleep"],
+        "Thursday" => ["Wake up", "Take a bath", "Go to school", "Eat lunch", "Go home", "Study", "Sleep"],
+        "Friday" => ["Wake up", "Take a bath", "Go to online class", "Eat lunch", "Play games", "Study", "Sleep"],
+        "Saturday" => ["Wake up", "Take a bath", "Eat breakfast", "Study", "Play football", "Rest", "Sleep"],
+        "Sunday" => ["Rest"],
+    ];
