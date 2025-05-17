ae647ead0486aa4791e6c7e822c9db78c7678106
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 23:07:48 +0800

    style(data handler) input missing closing tag

--- handlers/data.handler.php
+++ handlers/data.handler.php
@@ -8,5 +8,6 @@
         "Thursday" => ["Wake up", "Take a bath", "Go to school", "Eat lunch", "Go home", "Study", "Sleep"],
         "Friday" => ["Wake up", "Take a bath", "Go to online class", "Eat lunch", "Play games", "Study", "Sleep"],
         "Saturday" => ["Wake up", "Take a bath", "Eat breakfast", "Study", "Play football", "Rest", "Sleep"],
-        "Sunday" => ["Rest"],
+        "Sunday" => ["Go to church", "Rest"],
     ];
+?>
