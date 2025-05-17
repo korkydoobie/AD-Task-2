35ca7f2d4e2663657291079b30cf1a028deee384
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 23:57:35 +0800

    refactor(Landing page) modified div box for credential placeholder

--- index.php
+++ index.php
@@ -6,10 +6,9 @@
 </head>
     <body>
         <div class="login-box">
-            <div class="info-box">
-                <p><b>Username: </b> LeSunshine.Gems</p>
-                <p><b>Password: </b> **********</p>
-            </div>
+            <div class="credential">
+                <b>Username:</b> LeSunshine
+                <b>Password:</b> *********
         </div>
 
         <a href="/page/index.php" class="login-btn">Login</a>
