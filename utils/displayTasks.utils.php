433b35760bf69f1f128fe10ffd5f96762f2d59aa
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 19:20:53 +0800

    fix(display tasks util) fixed unordered list declaration typo

--- utils/displayTasks.utils.php
+++ utils/displayTasks.utils.php
@@ -1,7 +1,7 @@
 <?php
     function displayTasks($day, $taskPerDay){
         echo "<h2>$day's Tasks: </h2>";
-        echo "ul";
+        echo "<ul>";
 
         foreach($taskPerDay[$day] as [$task]){
             echo "<li> $task</li>";
