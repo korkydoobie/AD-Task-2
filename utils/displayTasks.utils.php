129a239951b2e88040cc64a0295dc40bb5a30ae5
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 19:00:52 +0800

    feat(display tasks util) added a function to display tasks per day

---
+++ utils/displayTasks.utils.php
@@ -0,0 +1,11 @@
+<?php
+    function displayTasks($day, $taskPerDay){
+        echo "<h2>$day's Tasks: </h2>";
+        echo "ul";
+
+        foreach($taskPerDay[$day] as [$task]){
+            echo "<li> $task</li>";
+        }
+        echo "</ul>";
+    }
+?>
