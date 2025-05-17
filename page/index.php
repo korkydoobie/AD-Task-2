3706996e5723bd2ea33ceead78073124a1bb4d01
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 19:12:51 +0800

    refactor(landing page) edited landing page structure for fragmentation

--- page/index.php
+++ page/index.php
@@ -6,9 +6,6 @@
     </head>
     <body>
         <?php
-            //declarations
-            $tasks = ["Wake up", "Eat breakfast", "Study", "Eat lunch", "Play games", "Sleep"];
-
             echo "<h1>Today's Tasks: </h1>";
             echo "<ul>";
 
