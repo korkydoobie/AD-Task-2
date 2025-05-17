8cf5b3effe7ec122251671fbf34cca1c0b83f1d9
kirkgriffin <krkgrffntn@gmail.com>
2025-05-17 23:07:06 +0800

    refactor(landing page) changed display style from expanding card to separate div section for task display

--- index.php
+++ index.php
@@ -2,15 +2,22 @@
 <html>
 <head>
     <title>Activity</title>
-    <!-- <link rel="stylesheet" href="assets/css/landingPage.css"> -->
+    <link rel="stylesheet" href="assets/css/landingPage.css">
 </head>
     <body>
-        <?php
-        REQUIRE_ONCE __DIR__ . '/handlers/data.handler.php';
-        REQUIRE_ONCE __DIR__ . '/utils/displayTasks.utils.php';
+        <div class="container">
+            <?php
+            REQUIRE_ONCE __DIR__ . '/handlers/data.handler.php';
+            REQUIRE_ONCE __DIR__ . '/utils/displayTasks.utils.php';
 
-        displayTasks($days, $tasks);
-        ?>
+            displayTasks($days, $tasks);
+            ?>
+            
+            <div id="task-display" class="task-display">
+                <h2 id="selected-day">Select a day</h2>
+                <ul id="selected-tasks"></ul>
+            </div>
+        </div>
         <script src="assets/js/script.js"></script>
     </body>
 </html>
